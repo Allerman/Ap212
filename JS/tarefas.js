d38b61764dcf23bc49ca8a89694b7779b8c95b72
allerman <allerman3301@gmail.com>
2023-04-04 12:43:46 -0300

    build: funçoes de add, dell, checar e salvar adicionadas

--- JS/tarefas.js
+++ JS/tarefas.js
@@ -1,104 +1,49 @@
-let inputNovaTarefa = document.querySelector('#inputNovaTarefa');
-let btnAddTarefa = document.querySelector('#btnAddTarefa');
-let listaTarefa = document.querySelector('#listaTarefa');
-let btnEdit = document.querySelector('#btnEdit');
-let btnSairEdicao = document.querySelector('#btnSairEdicao');
-let btnSalvarEdicao = document.querySelector('#btnSalvarEdicao');
-let tarefaEdicao = document.querySelector('#tarefaEdicao');
-let inputEdicaoNovoTexto = document.querySelector('#inputEdicaoNovoTexto');
-let mostrandoIdDaTarefa = document.querySelector('#mostrandoIdDaTarefa');
+const inputBox=document.getElementById("input-box");
+const listItens=document.getElementById("listItens");
 
-//adicionar tarefa
-inputNovaTarefa.addEventListener('keypress',(e)=>{
-    if(e.keyCode == 13  /*13 porque e a keycode do enter*/){
-        let tarefa = {
-            nome: inputNovaTarefa.value,
-            id: gerarid(),
-        }
-//chamando funçao
-    adicionarTarefa(tarefa);
-    } 
-})
-
-btnAddTarefa.addEventListener('click', (e) => {
-    let tarefa={ 
-        nome: inputNovaTarefa.value,
-        id: gerarid()
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Voce tem que adicinar algo!");
     }
-        adicionarTarefa(tarefa);})
-
-
-function adicionarTarefa(tarefa){
-    let li = criarTag(tarefa);
-    listaTarefa.appendChild(li);
-    inputNovaTarefa.value = '';
-}
-
-
-function criarTag(tarefa){
-
-    let li = document.createElement('li');
-    li.id = tarefa.id;
-
-    let span = document.createElement('span');
-    span.classList.add('texTarefa');
-    span.innerHTML = tarefa.nome;
-
-    let div = document.createElement('butao');
-
-    let btnEdit = document.createElement('button');
-    btnEdit.classList.add('btnAcao');
-    btnEdit.innerHTML = '<i class="bi bi-pencil-square"></i>';
-    btnEdit.setAttribute('onclick', 'edit('+tarefa.id+')');
-
-    let btnDel = document.createElement('button');
-    btnDel.classList.add('btnAcao');
-    btnDel.innerHTML = '<i class="bi bi-trash3-fill"></i>';
-    btnDel.setAttribute('onclick', 'delet('+tarefa.id+')');
-
-    div.appendChild(btnEdit);
-    div.appendChild(btnDel);
-
-    li.appendChild(span);
-    li.appendChild(div);
-    return li;
-}
-
-function alterarjanelaEdicao(){
-    btnEdit.addEventListener('click',function(){
-        
-        if(popupDaEdicao.style.display === 'block'){
-            popupDaEdicao.style.display='none';
-        }
-        else{
-            popupDaEdicao.style.display='block';}})
-}
-
-btnSairEdicao.addEventListener('click',(e)=>{
-    alterarjanelaEdicao();
-})
-
-btnSalvarEdicao.addEventListener('click',(e)=>{
-    e.preventDefault();
-    let idTarefa = mostrandoIdDaTarefa.innerHTML.replace('#','');
-    let tarefa = {
-        nome: inputNovaTarefa.value,
-        id: gerarid()
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listItens.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7"
+        li.appendChild(span);
     }
+    inputBox.value="";
+    saveData()
+}
+//adding with ENTER
+inputBox.addEventListener('keypress',(e)=>{
+    if(e.keyCode===13){
+        addTask();
+    }})
 
-})
 
-function delet (idTarefa){
-    let confirmacao = window.confirm('Deseja Deletar Tarefa?');
-    if(confirmacao) {
-        let li = document.getElementById(''+idTarefa+'');
-        if(li){
-            listaTarefa.removeChild(li);
-        }
-     }
+//pointing to task completion
+listItens.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+//deleting the task
+    else if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+},false);
+
+
+//saving the task in the browser
+function saveData(){
+    localStorage.setItem("data", listItens.innerHTML)
+}
+//showing the task in the browser
+function showTask(){
+    listItens.innerHTML=localStorage.getItem("data");
 }
 
-//funçao provisoria para criar id, pois irei adicionar um banco de dados
-function gerarid(){
-    return Math.floor(Math.random()*2000);
-}
+showTask();
