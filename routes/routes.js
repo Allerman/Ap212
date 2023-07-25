90c6e58a1116f0aaa985c7423232303a2e1b51be
allerman <allerman3301@gmail.com>
2023-07-25 02:45:49 -0300

    build: construçao de rotas

---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const HomeController = require("../controller/HomeController");
+const ItemsController =  require("../controller/ItemsController");
+
+router.get("/", HomeController.getAllItems);
+router.post("/Quarto", ItemsController.creatItemQuarto);
+router.post("/Cozinha", ItemsController.creatItemCozinha);
+router.post("/Lavanderia", ItemsController.creatItemLavanderia);
+router.post("/Banheiro", ItemsController.creatItemBanheiro);
+router.delete('/Delete/:id', HomeController.deleteItem);
+
+module.exports = router;
