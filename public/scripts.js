5446e5ca900c18b3077f505961a9f3ee27ade78e
allerman <allerman3301@gmail.com>
2023-07-25 23:58:36 -0300

    feat: anchor e accordionfunctions

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,5 @@
 const butDelet = document.querySelectorAll('.bntDelete');
-
+//delete
 butDelet.forEach(button =>{
     button.addEventListener('click',async ()=>{
         const itemId = button.getAttribute('data-id');
@@ -11,4 +11,51 @@ butDelet.forEach(button =>{
             console.log(err);
         };
     });
+});
+
+
+//post function anchor
+document.addEventListener("DOMContentLoaded", () => {
+    const anchor = window.location.hash;
+    if (anchor) {
+      const element = document.querySelector(anchor);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  });
+
+
+//accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+
+  var panel = acc[i].nextElementSibling;
+  var panelState = localStorage.getItem(panel.id);
+  if (panelState === "open") {
+    panel.style.display = "block";
+    acc[i].classList.add("active");
+  } else {
+    panel.style.display = "none";
+    acc[i].classList.remove("active");
+  }
+}
+
+window.addEventListener("beforeunload", function() {
+  for (i = 0; i < acc.length; i++) {
+    var panel = acc[i].nextElementSibling;
+    var panelState = panel.style.display === "block" ? "open" : "closed";
+    localStorage.setItem(panel.id, panelState);
+  }
 });
